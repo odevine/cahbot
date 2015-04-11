f07a9aefd768682e43b0e6d498bd0cd4ac019936
Owen <0d3vine@gmail.com>
2015-04-10 20:52:19 -0400

    make it better

--- blackcardarray.js
+++ blackcardarray.js
@@ -1,7 +1,10 @@
 var blackcards = [
  
 	"cahbot Don't forget to write down my new email address: _@_.com.",
-	
+	"cahbot -what You. Me. _. Now.",
+	"cahbot 911, -what's your emergency?",
+	"cahbot Oh no! _! My only weakness!",
+
 ]
 
 module.exports = blackcards;
