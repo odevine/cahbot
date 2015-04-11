1bed5cffd2aeb970bffaed9c7efe40c082e7c7e0
Owen <0d3vine@gmail.com>
2015-04-10 20:45:57 -0400

    make it better

---
+++ blackcardarray.js
@@ -0,0 +1,7 @@
+var blackcards = [
+ 
+	"cahbot Don't forget to write down my new email address: _@_.com.",
+	
+]
+
+module.exports = blackcards;
--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var HTTPS = require('https'),
     botID = process.env.BOT_ID,
-    everything = require('./whitecardarray.js'),
+    whitecard = require('./whitecardarray.js'),
+    blackcard = require('./blackcardarray.js'),
     who = require('./whoArray.js'),
     what = require('./whatArray.js'),
     action = require('./actionArray.js'),
@@ -12,10 +13,11 @@ function respond() {
         botRegex1 = /^cahbot.*_.*_/i,
         botRegex2 = /^cahbot options/i,
         botRegex3 = /^cahbot update/i,
-        botRegex4 = /^cahbot.*-who.*/i,
-        botRegex5 = /^cahbot.*-what.*/i,
-        botRegex6 = /^cahbot.*-act.*/i,
-        botRegex7 = /^cahbot/i,
+        botRegex4 = /^cahbot random/i,
+        botRegex5 = /^cahbot.*-who.*/i,
+        botRegex6 = /^cahbot.*-what.*/i,
+        botRegex7 = /^cahbot.*-act.*/i,
+        botRegex8 = /^cahbot/i,
         word1,
         word2;
 
@@ -23,8 +25,8 @@ function respond() {
     if(request.text && botRegex1.test(request.text)) {
         this.res.writeHead(200);
         setTimeout(function() {
-            word1 = getWord1(everything);
-            word2 = getWord2(everything);
+            word1 = getWord1(whitecard);
+            word2 = getWord2(whitecard);
             postMultiMessage(word1,word2);
             console.log('posted multi-word message!');
         }, 500);
@@ -35,7 +37,7 @@ function respond() {
     else if(request.text && botRegex2.test(request.text)){
         this.res.writeHead(200);
         setTimeout(function() {
-            postOptions();
+            postOther(menu);
             console.log('posted options!');
         }, 500);
         this.res.end();
@@ -44,7 +46,7 @@ function respond() {
     else if(request.text && botRegex3.test(request.text)){
         this.res.writeHead(200);
         setTimeout(function() {
-            postUpdate();
+            postOther(update);
             console.log('posted update!');
         }, 500);
         this.res.end();
@@ -53,13 +55,23 @@ function respond() {
     else if(request.text && botRegex4.test(request.text)){
         this.res.writeHead(200);
         setTimeout(function() {
+            word1 = getWord1(blackcard);
+            postOther(word1);
+            console.log('posted random!');
+        }, 500);
+        this.res.end();
+    }
+    //REGEX5
+    else if(request.text && botRegex5.test(request.text)){
+        this.res.writeHead(200);
+        setTimeout(function() {
             word1 = getWord1(who);
             postMessage(word1);
             console.log('posted who!');
         }, 500);
         this.res.end();
-    }//REGEX5
-    else if(request.text && botRegex5.test(request.text)){
+    }//REGEX6
+    else if(request.text && botRegex6.test(request.text)){
         this.res.writeHead(200);
         setTimeout(function() {
             word1 = getWord1(what);
@@ -67,8 +79,8 @@ function respond() {
             console.log('posted what!');
         }, 500);
         this.res.end();
-    }//REGEX6
-    else if(request.text && botRegex6.test(request.text)){
+    }//REGEX7
+    else if(request.text && botRegex7.test(request.text)){
         this.res.writeHead(200);
         setTimeout(function() {
             word1 = getWord1(action);
@@ -77,11 +89,11 @@ function respond() {
         }, 500);
         this.res.end();
     }
-    //REGEX7
-    else if(request.text && botRegex7.test(request.text)){
+    //REGEX8
+    else if(request.text && botRegex8.test(request.text)){
         this.res.writeHead(200);
         setTimeout(function() {
-            word1 = getWord1(everything);
+            word1 = getWord1(whitecard);
             postMessage(word1);
             console.log('posted single word message!');
         }, 500);
@@ -144,7 +156,7 @@ function postMultiMessage(a,b) {
     botReq.end(JSON.stringify(body));
 }
 
-function postOptions() {
+function postOther(a) {
     var options, body, botReq;
     
     options = {
@@ -155,10 +167,10 @@ function postOptions() {
 
     body = {
         "bot_id": botID,
-        "text": menu
+        "text": a
     };
 
-    console.log('sending ' + menu + ' to ' + botID);
+    console.log('sending ' + n + ' to ' + botID);
 
     botReq = HTTPS.request(options, function(res) {
         if(res.statusCode == 202) {
@@ -178,39 +190,6 @@ function postOptions() {
     botReq.end(JSON.stringify(body));
 }
 
-function postUpdate() {
-    var options, body, botReq;
-    
-    options = {
-        hostname: 'api.groupme.com',
-        path: '/v3/bots/post',
-        method: 'POST'
-    };
-
-    body = {
-        "bot_id": botID,
-        "text": update
-    };
-
-    console.log('sending ' + update + ' to ' + botID);
-
-    botReq = HTTPS.request(options, function(res) {
-        if(res.statusCode == 202) {
-        } else {
-            console.log('rejecting bad status code ' + res.statusCode);
-        }
-    });
-
-    botReq.on('error', function(err) {
-        console.log('error posting message '  + JSON.stringify(err));
-    });
-
-    botReq.on('timeout', function(err) {
-        console.log('timeout posting message '  + JSON.stringify(err));
-    });
-
-    botReq.end(JSON.stringify(body));
-}
 
 function postMessage(a) {
     var options, body, botReq;
