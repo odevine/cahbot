e4fa9d5e57f3f1017039197ff3f0622f544b1ee7
Owen <0d3vine@gmail.com>
2015-04-10 21:22:34 -0400

    make it better

--- blackcardarray.js
+++ blackcardarray.js
@@ -9,7 +9,7 @@ var blackcards = [
 	"cahbot I couldn't find a condom, so I had to use _ instead.",
 	"cahbot On a scale of _ to _, how would you rate your pain?",
 	"cahbot Yeah, I had to break with him. He kept using '_' as a euphemism for having sex.",
-	"cahbot I kissed a -what and I liked it!",
+	"cahbot I kissed a _ and I liked it!",
 	"cahbot The judge acquitted me because _ isn't technically a crime.",
 	"cahbot Do you know why I pulled you over?",
 	"cahbot Six Flag's new thrill ride lets you experience _ at 120 miles per hour.",
